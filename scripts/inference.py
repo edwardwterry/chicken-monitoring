1bd3eb334644619b51295c34002beed04adac39d
Edward Terry <edward.william.terry@gmail.com>
2020-07-26 19:02:49 -0700

    Continue updating inference

--- scripts/inference.py
+++ scripts/inference.py
@@ -8,6 +8,7 @@ import numpy as np
 import rospy
 import cv2
 import torch
+from PIL import Image
 from sensor_msgs.msg import Image as RosImage
 
 from models import *
@@ -20,62 +21,73 @@ from torchvision import datasets
 from torch.autograd import Variable
 
 br = CvBridge()
-
-
-def overlay_bb(im, x1, y1, x2, y2):
-    return cv2.rectangle(im, (x1, y1), (x2, y2), (0, 0, 255), 3)
-
-def color_clbk(msg):
-    im = br.imgmsg_to_cv2(msg)
-    im = torch.from_numpy(im)
-    im = Variable(im.type(Tensor))
-    with torch.no_grad():
-        detections = models[image_type](im)
-        detections = non_max_suppression(detections, conf_thres, nms_thres)
-    if detections is not None:
-        detections = rescale_boxes(detections, img_size, im.shape[:2])
-        for x1, y1, x2, y2, conf, cls_conf, cls_pred in detections:
-            im = overlay_bb(im, x1, y1, x2, y2)
-
-rospy.init_node('inference', anonymous=True)
-
-color_sub = rospy.Subscriber('color', RosImage, color_clbk)
-# thermal_sub = rospy.Subscriber('thermal', Image, thermal_clbk)
-
-# Read in parameters
-model_defs = {'color': None, 'thermal': None}
-weights = {'color': None, 'thermal': None}
-img_size = {'color': None, 'thermal': None}
-model_defs['color'] = rospy.get_param('model_color')
-model_defs['thermal'] = rospy.get_param('model_thermal')
-weights['color'] = rospy.get_param('weights_color')
-weights['thermal'] = rospy.get_param('weights_thermal')
-img_size['color'] = rospy.get_param('img_size_color')
-img_size['thermal'] = rospy.get_param('img_size_thermal')
-conf_thresh = rospy.get_param('conf_thresh')
-nms_thresh = rospy.get_param('nms_thresh')
-batch_size = rospy.get_param('batch_size')
-n_cpu = rospy.get_param('n_cpu')
-
-models = {'color': None, 'thermal': None}
-
 Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-for image_type in model_defs.keys():
-    models[image_type] = Darknet(model_defs[image_type], img_size=img_size[image_type]).to(device)
-    models[image_type].load_state_dict(torch.load(weights[image_type]))
-    models[image_type].eval()
+class Detection():
+    def __init__(self):
+        # Read in parameters
+        self.model_defs = {'color': None, 'thermal': None}
+        self.weights = {'color': None, 'thermal': None}
+        self.img_size = {'color': None, 'thermal': None}
+        self.model_defs['color'] = rospy.get_param('model_color')
+        self.model_defs['thermal'] = rospy.get_param('model_thermal')
+        self.weights['color'] = rospy.get_param('weights_color')
+        self.weights['thermal'] = rospy.get_param('weights_thermal')
+        self.img_size['color'] = rospy.get_param('img_size_color')
+        self.img_size['thermal'] = rospy.get_param('img_size_thermal')
+        self.conf_thresh = rospy.get_param('conf_thresh')
+        self.nms_thresh = rospy.get_param('nms_thresh')
+        self.batch_size = rospy.get_param('batch_size')
+        self.n_cpu = rospy.get_param('n_cpu')
+        self.preprocess = transforms.Compose([transforms.Resize(416), transforms.ToTensor()])
 
+        # Prepare pub/sub
+        self.color_sub = rospy.Subscriber('color', RosImage, self.color_clbk)
+        # thermal_sub = rospy.Subscriber('thermal', Image, thermal_clbk)
 
+        # Prepare models
+        self.models = {'color': None, 'thermal': None}
+
+        for image_type in ['color']: # model_defs.keys():
+            rospy.loginfo('Creating model: ' + image_type)
+            self.models[image_type] = Darknet(self.model_defs[image_type], img_size=self.img_size[image_type]).to(device)
+            rospy.loginfo('Loading weights...')
+            self.models[image_type].load_state_dict(torch.load(self.weights[image_type]))
+            self.models[image_type].eval()
+            rospy.loginfo('Model preparation complete!')
+
+    def overlay_bb(self, im, x1, y1, x2, y2):
+        return cv2.rectangle(im, (x1, y1), (x2, y2), (0, 0, 255), 3)
+
+    def color_clbk(self, msg):
+        im = br.imgmsg_to_cv2(msg)
+        im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+        # https://stackoverflow.com/questions/43232813/convert-opencv-image-format-to-pil-image-format
+        im_pil = Image.fromarray(im)
+        im_pil = self.preprocess(im_pil)
+        # im_pil = transforms.ToTensor()(im_pil.convert('RGB')).unsqueeze(0).cuda()
+        # im_pil = Variable(im_pil.type(Tensor))
+        with torch.no_grad():
+            print('Running inference!')
+            detections = self.models['color'](im_pil)
+            detections = non_max_suppression(detections, conf_thres, nms_thres)
+        if detections is not None:
+            detections = rescale_boxes(detections, self.img_size['color'], im_pil.shape[:2])
+            print(detections)
+            # for x1, y1, x2, y2, conf, cls_conf, cls_pred in detections:
+                # im = self.overlay_bb(im, x1, y1, x2, y2)
 # def thermal_clbk(msg):
 #     im = br.imgmsg_to_cv2(msg)
 #     im = transforms.ToTensor()(im.convert('1'))
 #     with torch.no_grad():
 #         detections = models[image_type](im)
 
-# def main(args):
+def main(args):
+    rospy.init_node('inference', anonymous=True)
+    det = Detection()
+    while not rospy.is_shutdown():
+        rospy.spin()
 
-# if __name__ == '__main__':
-#     main(sys.argv)    
+if __name__ == '__main__':
+    main(sys.argv)    
